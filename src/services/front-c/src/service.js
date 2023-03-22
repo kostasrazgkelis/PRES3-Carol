0c17e13fc48edfcfe19f029f8fbad5a53a166a37
KoRaZ21 <konstantinos.razgkelis@onelity.com>
2023-03-22 15:38:21 +0200

    Fixed argument

--- src/services/front-c/src/service.js
+++ src/services/front-c/src/service.js
@@ -1,19 +1,10 @@
 import axios from "axios";
 
-// export const getFilesA = async () =>
-//   await axios.get("http://localhost:9500/show-files?directory=pretransformed_data", null, {});
-//
-// export const getFilesB = async () =>
-//   await axios.get("http://snf-33343.ok-kno.grnetcloud.net:9500/show-files?directory=pretransformed_data", null, {});
-//
-// export const joinFiles = async (postRes) =>
-//   await axios.post("http://snf-33344.ok-kno.grnetcloud.net:9000/start", postRes);
-
 export const getFilesA = async () =>
   await axios.get(process.env.REACT_APP_URI_HOST_SHOW_FILES_A, null, {});
 
 export const getFilesB = async () =>
-  await axios.get(process.env.REACT_APP_URI_HOST_SHOW_FILES_A, null, {});
+  await axios.get(process.env.REACT_APP_URI_HOST_SHOW_FILES_B, null, {});
 
 export const joinFiles = async (postRes) =>
   await axios.post(process.env.REACT_APP_JOIN_FILES_A_B, postRes);
